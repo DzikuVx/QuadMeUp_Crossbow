db1cd344cb78c4e30136baddae88ab662f90260b
Pawel Spychalski <pawel.spychalski@raisin.com>
2018-04-10 08:53:15 +0200

    some config optimizations

--- crossbow/config.h
+++ crossbow/config.h
@@ -1,12 +1,30 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/*
+ *  Hardware type. Available types:
+ *  ARDUINO_AVR_FEATHER32U4
+ *  ARDUINO_SAMD_FEATHER_M0
+ */
+#define ARDUINO_AVR_FEATHER32U4
+
+/*
+ * TX or RX mode for hardware. Available types:
+ * DEVICE_MODE_TX
+ * DEVICE_MODE_RX
+ */
 #define DEVICE_MODE_TX
 // #define DEVICE_MODE_RX
 
-#define FEATURE_TX_OLED
+// #define FEATURE_TX_OLED
 // #define FORCE_TX_WITHOUT_INPUT
-// #define FEATURE_TX_INPUT_PPM
+
+/*
+ * Default mode of TX data input: SBUS
+ * Possible values:
+ * FEATURE_TX_INPUT_PPM
+ * FEATURE_TX_INPUT_SBUS
+ */
 #define FEATURE_TX_INPUT_SBUS
 
 // #define DEBUG_SERIAL
